073149ea5c6234a8f05b89d725993f6077bda75b
kashish1168 <112888608+kashish1168@users.noreply.github.com>
2024-12-10 19:28:05 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,54 @@
+const data=[
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra is a city in northen India, known for the iconic Taj Mahal. Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals in the early 16th century. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw",
+        image: "https://cdn.thecodehelp.in/Agra.jpeg",
+        price: "35,758",
+    },
+    {
+        id:2,
+        name:"Varanasi",
+        info:"Varanasi is one of the world's oldest continually inhabited cities.[21][22][23] Kashi, its ancient name, was associated with a kingdom of the same name of 2,500 years ago. The Lion capital of Ashoka at nearby Sarnath has been interpreted to be a commemoration of the Buddha's first sermon there in the fifth century BCE",
+         image:"https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price: "25,758",
+    },
+    {
+        id:3,
+        name:"Jaipur",
+        info:"Jaipur was founded in 1727 by Sawai Jai Singh II, the Kachhwaha Rajput ruler of Amer, after whom the city is named.[12] It is one of the earliest planned cities of modern India, designed by Vidyadhar Bhattacharya.[13] During the British colonial period, the city served as the capital of Jaipur State. After Indian independence in 1947, Jaipur was made the capital of the newly formed state of Rajasthan in 1949.",
+        image:"https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price: "85,758",
+    },
+    {
+        id:4,
+        name:"Goa",
+        info:"Goa is visited by large numbers of international and domestic tourists each year because of its white-sand beaches, active nightlife, places of worship, and World Heritage-listed architecture. It also has rich flora and fauna because it lies very close to the North Western Ghats rainforests, one of the rare biodiversity hotspots of the world.",
+        image:"https://cdn.thecodehelp.in/Goa.jpeg",
+        price: "95,758",
+    },
+    {
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+		price: "78,595",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+		price: "68,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+		price: "68,595",
+	},
+
+]
+
+export default data;
